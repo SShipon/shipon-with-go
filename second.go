39ef0b7985cb2c9ba1e7b84c1c11c31b4ef73eb0
oxshipon1@gmail.com <oxshipon1@gmail.com>
2025-09-09 09:42:47 +0600

    go data type

---
+++ second.go
@@ -0,0 +1,14 @@
+package main
+import ("fmt")
+// data type in golang 
+func main() {
+  var a bool = true    // Boolean
+  var b int = 5        // Integer
+  var c float32 = 3.14 // Floating point number
+  var d string = "Hi!" // String
+
+  fmt.Println("Boolean: ", a)
+  fmt.Println("Integer: ", b)
+  fmt.Println("Float:   ", c)
+  fmt.Println("String:  ", d)
+}
